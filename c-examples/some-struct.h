8851e247885aa3122a3c1b5adc5d60edcc422314
wenbin <root@quwenbin.localdomain>
2021-03-09 10:17:13 +0800

    modify

---
+++ c-examples/some-struct.h
@@ -0,0 +1,9 @@
+#pragma one
+
+typedef struct some_struct* SOME_STRUCT_PTR;
+
+extern SOME_STRUCT_PTR new_some_struct(void);
+
+extern int get_num(SOME_STRUCT_PTR s);
+
+extern char* get_str(SOME_STRUCT_PTR s);
--- temlates/appendix/concepts/concepts_learn_v1.cpp
+++
@@ -1,4 +0,0 @@
-template <typename T>
-concept LessThanComparable = requires(T x, T y) {
-  { x < y } -> bool;
-};
